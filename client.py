ae5a37e795f8fd3c33cc466b4051b01017be4ff1
Melker Öberg <memleoberg@gmail.com>
2023-10-09 04:38:58 +0200

    stable

--- client.py
+++ client.py
@@ -14,14 +14,27 @@ except socket.error as e:
 print("Connected to the server. Enter your commands below:")
 print("Enter 'exit' to close the connection.")
 
+def send_msg(sock, msg):
+    msg_encoded = msg.encode()
+    msg_length = len(msg_encoded)
+    msg_length_encoded = msg_length.to_bytes(4, 'big')
+    sock.sendall(msg_length_encoded)
+    sock.sendall(msg_encoded)
+
+def recv_msg(sock):
+    msg_length_encoded = sock.recv(4)
+    if not msg_length_encoded:
+        return None
+    msg_length = int.from_bytes(msg_length_encoded, 'big')
+    msg_encoded = sock.recv(msg_length)
+    return msg_encoded.decode()
+
 while True:
     command = input("PS> ")
-    
     if command.lower() == "exit":
         break
-
-    client_socket.send(command.encode())
-    output = client_socket.recv(4096).decode()
+    send_msg(client_socket, command)
+    output = recv_msg(client_socket)
     print(output)
 
 client_socket.close()
--- main.py
+++ main.py
@@ -12,13 +12,26 @@ server_socket.listen(5)
 
 print(f"Server is listening on {HOST}:{PORT}")
 
-def handle_client(client_socket):
-    working_dir = os.getcwd()  # Always initialize working_dir
+def send_msg(sock, msg):
+    msg_encoded = msg.encode()
+    msg_length = len(msg_encoded)
+    msg_length_encoded = msg_length.to_bytes(4, 'big')
+    sock.sendall(msg_length_encoded)
+    sock.sendall(msg_encoded)
 
+def recv_msg(sock):
+    msg_length_encoded = sock.recv(4)
+    if not msg_length_encoded:
+        return None
+    msg_length = int.from_bytes(msg_length_encoded, 'big')
+    msg_encoded = sock.recv(msg_length)
+    return msg_encoded.decode()
+
+def handle_client(client_socket):
+    working_dir = os.getcwd()
     while True:
         try:
-            command = client_socket.recv(1024).decode()
-
+            command = recv_msg(client_socket)
             if not command:
                 break
 
@@ -36,58 +49,15 @@ def handle_client(client_socket):
                 if not response:
                     response = "Command executed with no output."
 
-            client_socket.send(response.encode())
+            send_msg(client_socket, response)
 
         except socket.error as e:
             print(f"Socket error: {e}")
             break
-        except Exception as e:
-            response = f"Server error: {e}"
-            client_socket.send(response.encode())  # Send error back to client without terminating the server
-            break
 
     client_socket.close()
-
 
 while True:
     client_socket, _ = server_socket.accept()
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
     client_thread.start()
-
-
-
-
-'''
-My current overview of what to do next
-
-    - Making sure the command gets sent and received correctly 
-        - What happens if a packet is lost? 
-            - I don't fully understand how the program handles this currently.
-            - Client should be informed and the server should not run the command
-
-            
-    - Ability to exit the server script just like the client script
-        - Simply mirror how the client script does it 
-
-    - Print the received command on server
-        - It would be useful to keep track of what commands the client has sent to the server 
-            either by printing it to terminal or by creating a file for every session created
-            (I dont know which is best)  
-
-
-    - Adding the ability to connect from another network 
-        - This step is important for the main idea of the project but introduces the highest security risk 
-            which is why I will integrate this the last thing I do 
-            (ignore this for now unless you have some good input on how I should integrate this)
-
-    - validating connecting device (should only my laptop and phone)
-        - What I'm thinking for the device validation is to only accept 
-            connection from my laptop and phones IP, but if there is a better way of doing this do that
-
-    
-            
-    I have no clue when it comes to Authentication and Encryption, but these are probably the most important   
-    - Authentication
-    - Encryption
-
-'''
