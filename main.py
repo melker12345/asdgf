cd719e494a8d74235bd8c8e4358047f4e509a336
Melker Öberg <memleoberg@gmail.com>
2023-10-09 03:58:26 +0200

    stable

--- main.py
+++ main.py
@@ -53,3 +53,41 @@ while True:
     client_socket, _ = server_socket.accept()
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
     client_thread.start()
+
+
+
+
+'''
+My current overview of what to do next
+
+    - Making sure the command gets sent and received correctly 
+        - What happens if a packet is lost? 
+            - I don't fully understand how the program handles this currently.
+            - Client should be informed and the server should not run the command
+
+            
+    - Ability to exit the server script just like the client script
+        - Simply mirror how the client script does it 
+
+    - Print the received command on server
+        - It would be useful to keep track of what commands the client has sent to the server 
+            either by printing it to terminal or by creating a file for every session created
+            (I dont know which is best)  
+
+
+    - Adding the ability to connect from another network 
+        - This step is important for the main idea of the project but introduces the highest security risk 
+            which is why I will integrate this the last thing I do 
+            (ignore this for now unless you have some good input on how I should integrate this)
+
+    - validating connecting device (should only my laptop and phone)
+        - What I'm thinking for the device validation is to only accept 
+            connection from my laptop and phones IP, but if there is a better way of doing this do that
+
+    
+            
+    I have no clue when it comes to Authentication and Encryption, but these are probably the most important   
+    - Authentication
+    - Encryption
+
+'''
