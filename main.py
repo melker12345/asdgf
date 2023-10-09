06a4f68d69d22411c645804307661df3a17c1f8c
Melker Öberg <memleoberg@gmail.com>
2023-10-09 05:45:59 +0200

    stable

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import select
 import socket
+import sys
 import threading
 import subprocess
 import os
 
+# Configuration
 HOST = '0.0.0.0'
 PORT = 8080
+shutdown_flag = False  # Flag for graceful shutdown
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
@@ -12,6 +16,7 @@ server_socket.listen(5)
 
 print(f"Server is listening on {HOST}:{PORT}")
 
+#line 19
 def send_msg(sock, msg):
     msg_encoded = msg.encode()
     msg_length = len(msg_encoded)
@@ -19,6 +24,7 @@ def send_msg(sock, msg):
     sock.sendall(msg_length_encoded)
     sock.sendall(msg_encoded)
 
+#line 27
 def recv_msg(sock):
     msg_length_encoded = sock.recv(4)
     if not msg_length_encoded:
@@ -27,6 +33,7 @@ def recv_msg(sock):
     msg_encoded = sock.recv(msg_length)
     return msg_encoded.decode()
 
+#line 36
 def handle_client(client_socket):
     working_dir = os.getcwd()
     while True:
@@ -37,14 +44,16 @@ def handle_client(client_socket):
 
             if command.startswith("cd "):
                 try:
-                    new_dir = os.path.join(working_dir, command.split(' ', 1)[1])
+                    new_dir = os.path.join(
+                        working_dir, command.split(' ', 1)[1])
                     os.chdir(new_dir)
                     working_dir = os.getcwd()
                     response = f"Changed directory to {working_dir}"
                 except Exception as e:
                     response = f"Error changing directory: {e}"
             else:
-                result = subprocess.run(['powershell', '-NoProfile', '-Command', command], cwd=working_dir, capture_output=True, text=True)
+                result = subprocess.run(
+                    ['powershell', '-NoProfile', '-Command', command], cwd=working_dir, capture_output=True, text=True)
                 response = result.stdout + result.stderr
                 if not response:
                     response = "Command executed with no output."
@@ -57,7 +66,33 @@ def handle_client(client_socket):
 
     client_socket.close()
 
-while True:
-    client_socket, _ = server_socket.accept()
-    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-    client_thread.start()
+#line 69
+def listen_for_exit_command():
+    global shutdown_flag
+    while True:
+        user_input = input(
+            "Type 'exit' to shut down the server: ").strip().lower()
+        if user_input == "exit":
+            shutdown_flag = True
+            print("\nShutting down the server...")
+            break
+
+# Start the input listener thread (add around Line 80)
+input_thread = threading.Thread(target=listen_for_exit_command)
+input_thread.daemon = True  # So that this thread will exit when the main program exits
+input_thread.start()
+
+
+# line 86
+try:
+    while not shutdown_flag:
+        # Check if there's a client trying to connect
+        readable, _, _ = select.select([server_socket], [], [], 1)  # Line 89
+
+        if server_socket in readable:
+            client_socket, _ = server_socket.accept()               # Line 91
+            client_thread = threading.Thread(
+                target=handle_client, args=(client_socket,))       # Line 93
+            client_thread.start()
+finally:
+    server_socket.close() 
